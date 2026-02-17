5abbea70d9dd500a9da154b6e2d4a98c2a7d9077
Kayden Lancaster <a02146555@usu.edu>
2026-02-17 15:29:24 -0700

    Begin implementation.

---
+++ Scripts/WaveFunctionCollapse.cs
@@ -0,0 +1,11 @@
+using Godot;
+
+public class WaveFunctionCollapse
+{
+    private int[,] _grid;
+    private ulong[,] _superPosition;
+    public static int[,] Generate(int width, int height, ulong initialSuperposition)
+    {
+        return _grid;
+    }
+}
